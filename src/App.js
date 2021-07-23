18a539fa6c4b33e660a72de3ba2dee0d161de787
Dogancan Ulgu <dogancanulgu@gmail.com>
2021-07-24 02:14:39 +0300

    react-router was worked.

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import {
   BrowserRouter as Router,
   Switch,
   Route,
-  Link
+  NavLink
+
 } from "react-router-dom";
 
 import Home from "./components/Home"
 import About from "./components/About"
 import Users from "./components/Users"
+import Error404 from "./components/Error404"
 
 function App() {
   return (
@@ -18,20 +20,21 @@ function App() {
         <nav>
           <ul>
             <li>
-              <Link to="/">Home</Link>
+              <NavLink activeClassName="active" to="/" exact>Home</NavLink>
             </li>
             <li>
-              <Link to="/about">About</Link>
+              <NavLink activeClassName="active" to="/about">About</NavLink>
             </li>
             <li>
-              <Link to="/users">Users</Link>
+              <NavLink activeClassName="active" to="/users">Users</NavLink>
             </li>
           </ul>
         </nav>
         <Switch>
+          <Route path="/" exact component={Home} />
           <Route path="/about" component={About} />
           <Route path="/users" component={Users} />
-          <Route path="/" component={Home} />
+          <Route path="*" component={Error404} />
         </Switch>
       </div>
     </Router>
